e6d0888a3973d5be0d2f2a7c52ff1dd611f02732
Gradlon von Känel <gradlon@integral-systems.ch>
2022-05-08 22:24:02 +0200

    added Chatbot

--- src/lib/components/Chat.svelte
+++ src/lib/components/Chat.svelte
@@ -1,14 +1,26 @@
+<LibLoader url="https://app.chaport.com/javascripts/insert.js"
+on:loaded="{onLoaded}" />
+
 <script>
-	import { onMount } from 'svelte';
+    import LibLoader from './LibLoader.svelte';
+	import { onMount, createEventDispatcher } from 'svelte';
     import { browser } from "$app/env";
+
+    function onLoaded() {
+        console.log(window.chatport)
+        
+        window.chaportConfig = { 
+            appId : '627805c47489e7c5d08f9289',
+        };
+    }
 
 	onMount(async () => {
 		if (browser) {
-            window.chaportConfig = { 
-                appId : '627805c47489e7c5d08f9289',
-                openIn: '_blank',
-                autoStart: false
-            };
+            (function(w,d,v3){
+                w.chaportConfig = {
+                appId : '627805c47489e7c5d08f9289'
+                };
+                if(w.chaport)return;v3=w.chaport={};v3._q=[];v3._l={};v3.q=function(){v3._q.push(arguments)};v3.on=function(e,fn){if(!v3._l[e])v3._l[e]=[];v3._l[e].push(fn)};var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://app.chaport.com/javascripts/insert.js';var ss=d.getElementsByTagName('script')[0];ss.parentNode.insertBefore(s,ss)})(window, document);
         }
 	});
 </script>
